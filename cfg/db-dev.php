48ae2c010385cc0701339899e2c6e2741120d48c
yamin <yamin13y@gmail.com>
2024-12-17 19:29:29 +0100

    [task] delete password from the config file

--- cfg/db-dev.php
+++ cfg/db-dev.php
@@ -19,5 +19,5 @@ class ConfigDb
 // $dbConfig['host'] = 'mysql-motiv.alwaysdata.net';
 // $dbConfig['port'] = '3306';
 // $dbConfig['dbname'] = 'motiv_bdd';
-// $dbConfig['user'] = 'motiv';
-// $dbConfig['password'] = 'Greta-013';
+// $dbConfig['user'] = '';
+// $dbConfig['password'] = '';
